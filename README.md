bc373ad5de245aef73d2d8664b5d0543a954d988
Kishanlal B J <kishanlalbj@gmail.com>
2021-02-13 12:42:07 +0530

    fix: basic counter app

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
+![Master](https://github.com/kishanlalbj/blog-ui/workflows/Master/badge.svg) [![Coverage](https://sonarcloud.io/api/project_badges/measure?project=kishanlalbj_new-blog&metric=coverage)](https://sonarcloud.io/dashboard?id=kishanlalbj_new-blog)
+
 # Blog UI
--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,67 @@
-import React from "react";
-import { shallow } from "enzyme";
-import App from "./App";
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from './App';
 
-describe("rending app", () => {
-  it("should renderapp", () => {
-    const wrapper = shallow(<App />);
-    expect(wrapper.length).toBe(1);
+const setUp = (props = {}) => {
+  const component = shallow(<App {...props}></App>);
+  return component;
+};
+
+const findTestByAttr = (wrapper, val) => wrapper.find(`[data-test='${val}']`);
+
+describe('rending app', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = setUp();
+  });
+  it('should render app', () => {
+    const app = findTestByAttr(wrapper, 'app');
+    expect(app.length).toBe(1);
   });
 
-  it("should throw error", () => {
-    const wrapper = shallow(<p />);
-    expect(wrapper.length).toBe(1);
+  it('should display the counter', () => {
+    const counter = findTestByAttr(wrapper, 'app-counter');
+    expect(counter.length).toBe(1);
+  });
+});
+
+describe('Increment Counter', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = setUp();
+  });
+  it('should display the increment counter button', () => {
+    const button = findTestByAttr(wrapper, 'app-button-increment');
+    expect(button.length).toBe(1);
+  });
+
+  it('should increment the counter', () => {
+    const incrementButton = findTestByAttr(wrapper, 'app-button-increment');
+    incrementButton.simulate('click');
+
+    const count = findTestByAttr(wrapper, 'app-counter').text();
+    expect(Number(count)).toBe(1);
+  });
+});
+
+describe('Decrement counter', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = setUp();
+  });
+
+  it('should display the button  decrement', () => {
+    const decrementCounter = findTestByAttr(wrapper, 'app-button-decrement');
+
+    expect(decrementCounter.length).toBe(1);
+  });
+
+  it('should decrement the counter', () => {
+    const decrementCounter = findTestByAttr(wrapper, 'app-button-decrement');
+
+    decrementCounter.simulate('click');
+    const count = findTestByAttr(wrapper, 'app-counter').text();
+    expect(Number(count)).toBe(-1);
   });
 });
